da9c68823797d81d72dd36de27de802eaaecb98c
UlrikaN <ulrika@AD.SSIS.NU>
2017-09-26 14:45:36 +0200

    making states for cards

--- src/game.js
+++ src/game.js
@@ -2,28 +2,35 @@ import React from "react"
 import './game.css'
 import Card from "./card"
 
-const Game = () => (
-  <div className="game">
-    <h1>Ulrika's memory game</h1>
-    <div className="cardArea">
-      <Card src="/images/bild1.jpg"/>
-      <Card src="/images/bild2.jpg"/>
-      <Card src="/images/bild3.jpg"/>
-      <Card src="/images/bild4.jpg"/>
-      <Card src="/images/bild5.jpg"/>
-      <Card src="/images/bild6.jpg"/>
-      <Card src="/images/bild7.jpg"/>
-      <Card src="/images/bild8.jpg"/>
-      <Card src="/images/bild1.jpg"/>
-      <Card src="/images/bild2.jpg"/>
-      <Card src="/images/bild3.jpg"/>
-      <Card src="/images/bild4.jpg"/>
-      <Card src="/images/bild5.jpg"/>
-      <Card src="/images/bild6.jpg"/>
-      <Card src="/images/bild7.jpg"/>
-      <Card src="/images/bild8.jpg"/>
-    </div>
-  </div>
-)
+class Game extends React.Component {
+
+  state = {
+
+    cards: [
+      {src: "/images/bild1.jpg"},
+      {src: "/images/bild2.jpg"},
+      {src: "/images/bild3.jpg"},
+      {src: "/images/bild4.jpg"},
+      {src: "/images/bild5.jpg"},
+      {src: "/images/bild6.jpg"},
+      {src: "/images/bild7.jpg"},
+      {src: "/images/bild8.jpg"}
+    ]
+  }
+
+  render() {
+    return (
+      <div className="game">
+        <h1>Ulrika's memory game</h1>
+        <div className="cardArea">
+          {this.state.cards.map((card) => (
+            <Card src={card.src}/>
+          ))}
+        </div>
+      </div>
+    )
+  }
+
+}
 
 export default Game
